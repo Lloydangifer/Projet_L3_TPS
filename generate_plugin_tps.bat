c92b7dbfa12fa401a7901e4c7b3b7de7236e54ea
Quentin <Unniteq@gmail.com>
2015-11-04 18:15:47 +0100

    added directories to gitignore and added .bat

---
+++ generate_plugin_tps.bat
@@ -0,0 +1,11 @@
+mkdir build_tps
+cd build_tps
+
+REM on veut que les extensions sortent dans le dossier "Extensions" à côté de l'éditeur
+SET OUTPUT_DIR=C:\Shine\ShGameEditor\Extensions\
+
+REM on cherche le dossier Include et Lib fournit dans le zip "SDK"
+SET SHINE_DIR=C:\Shine\ShSDK\
+
+REM génération du project Visual Studio
+cmake -G "Visual Studio 10 2010" -DSH_ROOT_DIR=%SHINE_DIR% -DCMAKE_RUNTIME_OUTPUT_DIRECTORY_DEBUG=%OUTPUT_DIR% -DCMAKE_RUNTIME_OUTPUT_DIRECTORY_RELEASE=%OUTPUT_DIR% ../plugin-tps
